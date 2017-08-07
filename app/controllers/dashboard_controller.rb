4ef8d4de775c205b083b5b65faf5e0a55fb157d0
ramadhanfebbry <ramadhanfebbry@gmail.com>
2017-08-07 09:36:59 +0700

    update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,9 +13,9 @@ class DashboardController < ApplicationController
 
     if(params["start_date"].present? && params["end_date"].present?)
       @hps =  @hps.where("created_at > ? AND created_at < ?", "#{params['start_date']} 00:00:00", "#{params['end_date']} 00:00:00")
-    elsif (params[:start_date].present? && !params[:end_date].present?)
+    elsif (params['start_date'].present? && !params['end_date'].present?)
       @hps =  @hps.where("created_at > ?", "#{params['start_date']} 00:00:00")
-    elsif (!params[:start_date].present? && params[:end_date].present?)
+    elsif (!params['start_date'].present? && params['end_date'].present?)
       @hps =  @hps.where("created_at < ?", "#{params['end_date']} 00:00:00")
     else
     end
